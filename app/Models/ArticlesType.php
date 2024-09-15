d36d5b36343b61625eb3013359437c1e850d7c7b
aeiac-home <aeiac@qq.com>
2024-09-15 16:23:17 +0800

    add-Articles or ArticlesType Table Models

---
+++ app/Models/ArticlesType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArticlesType extends  Model
+{
+    protected $table = 'articles_type';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+}
---
+++ app/Models/Articlesr.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Articlesr extends  Model
+{
+    protected $table = 'articles';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+}
