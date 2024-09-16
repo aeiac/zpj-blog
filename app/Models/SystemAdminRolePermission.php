24b9198c8b6de06bf68f4f2aae344db21d496249
aeiac-home <aeiac@qq.com>
2024-09-17 00:05:32 +0800

    add-Model Table(System_Admin>>role_permission/)

---
+++ app/Models/SystemAdminRolePermission.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemAdminRolePermission extends Model
+{
+    protected $table = "system_admin_role_permission";
+
+    public $timestamps = true;
+}
