76c8325895eb8077c818ad2b4547615aa819dcc3
aeiac-home <aeiac@qq.com>
2024-10-18 17:28:19 +0800

    add-fun

--- app/Models/AdminUsers.php
+++ app/Models/AdminUsers.php
@@ -2,16 +2,33 @@
 
 namespace App\Models;
 
+
+use App\Models\Systems\SystemAdminRole;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use  \Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Systems\SystemAdminUsersRole;
 
 /**
  * @method static first()
- * @method static where(string $string, mixed $name)
+ * @method static where()
  * @method static find(string $adminUserId)
  */
 class AdminUsers extends Model
 {
     protected $table = 'admin_users';
     protected $primaryKey = 'id';
+
     public $timestamps = true;
+
+    public function userRoles(): HasMany
+    {
+        return $this->hasMany(SystemAdminUsersRole::class, 'users_id')
+            ->select(['id', 'users_id', 'role_id'])
+            ->with('roleDetail');
+    }
+
+
 }
+
+
