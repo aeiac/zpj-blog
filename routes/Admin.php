7c60f1d8878e5e6023896ad95f9dc4d9602777d4
aeiac-home <aeiac@qq.com>
2024-12-06 19:38:49 +0800

    Router-opt-路由命名增强

--- routes/Admin.php
+++ routes/Admin.php
@@ -2,34 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Admin\LoginAdminUsersController;
-use App\Http\Controllers\Admin\SystemAdminServicesController;
-use App\Http\Controllers\Admin\SystemAdminPermissionController;
-use App\Http\Controllers\Admin\ArticlesAdminController;
+use App\Http\Controllers\Admin\LoginBaseAdminUsersController;
+use App\Http\Controllers\Admin\SystemBaseAdminServicesController;
+use App\Http\Controllers\Admin\SystemBaseAdminPermissionController;
+use App\Http\Controllers\Admin\ArticlesBaseAdminController;
 
 // 登录模块
 Route::prefix('login')->group(function () {
-    Route::post('/', [LoginAdminUsersController::class, 'login'])->name('admin.login');
-    Route::post('/out', [LoginAdminUsersController::class, 'out'])->name('admin.logout');
+    Route::post('/', [LoginBaseAdminUsersController::class, 'login'])->name('admin.login');
+    Route::post('/out', [LoginBaseAdminUsersController::class, 'out'])->name('admin.logout');
 });
 
 // 系统配置模块
 Route::prefix('system')->group(function () {
     // IP封禁功能
     Route::prefix('blacklist')->group(function () {
-        Route::get('/list', [SystemAdminServicesController::class, 'blackList'])->name('admin.system.black.list');
-        Route::post('/save', [SystemAdminServicesController::class, 'blackListSave'])->name('admin.system.black.save');
+        Route::get('/list', [SystemBaseAdminServicesController::class, 'blackList'])->name('admin.system.black.list');
+        Route::post('/save', [SystemBaseAdminServicesController::class, 'blackListSave'])->name('admin.system.black.save');
     });
 });
 
 // 权限管理模块
 Route::prefix('permission')->group(function () {
     // 管理员管理
-    Route::get('/admin/users/list', [SystemAdminPermissionController::class, 'adminUserList'])->name('admin.permission.admin.users.list');
+    Route::get('/admin/users/list', [SystemBaseAdminPermissionController::class, 'adminUserList'])->name('admin.permission.admin.users.list');
 });
 
 // 文章管理模块
 Route::prefix('article')->group(function () {
-    Route::get('/list', [ArticlesAdminController::class, 'list'])->name('admin.article.list');
+    Route::get('/list', [ArticlesBaseAdminController::class, 'list'])->name('admin.article.list');
 
 });
