7e93260c5b9bc5fe6f4f33811a426c36cbc67c60
aeiac-home <aeiac@qq.com>
2024-12-06 15:20:42 +0800

    admin-add-黑名单操作优化

--- app/Http/Services/Admin/Systems/SystemAdminServices.php
+++ app/Http/Services/Admin/Systems/SystemAdminServices.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services\Admin\Systems;
 
 use App\Models\Systems\SystemBlackList;
+use App\Const\Admin\RedisKeyConst;
 use Illuminate\Support\Arr;
 
 class SystemAdminServices
@@ -38,22 +39,37 @@ class SystemAdminServices
     }
 
     /**
-     * 操作IP封禁列表（新增、修改）
-     * @param array $input
-     * @return array
+     * 新增或修改IP封禁记录
+     *
+     * @param array $input 需要操作的数据，包括：
+     * - id: 封禁记录的ID
+     * - ip_address: IP地址（选填）
+     * - reason: 封禁原因（选填）
+     * - status: 封禁状态（选填，1启用，0禁用）
+     *
+     * @return array 操作结果，包括：
+     * - message: 操作成功后的提示信息，包含当前操作的ID
+     * - code: 错误码（若有错误）
+     *
      */
     public function savaBlackList(array $input): array
     {
         $id = $input['id'] ?? null;
-        if (!empty($id)) {
-            $IpExists = SystemBlackList::where(['ip', '=', $input['ip']])->exists();
-            return $IpExists ?? ['message' => '当前IP已存在！'];
+        if (!empty($input['ip_address'])) {
+            $IpExists = SystemBlackList::where([
+                'ip_address' => $input['ip_address'],
+                'status' => SystemBlackList::STATUS_ACTIVE
+            ])->exists();
+            if ($IpExists) {
+                return ['code'=>401,'message' => '当前IP已存在！'];
+            }
         }
-        $input = Arr::only($input, [
-            'ip', 'reason', 'status'
-        ]);
+        $input = Arr::only($input, ['ip_address', 'reason', 'status']);
         $component = SystemBlackList::updateOrCreate(['id' => $id], $input);
-        return ['message' => '记录成功！当前ID为：' . $component->id];
+        if ($component->id && $input['status'] == SystemBlackList::STATUS_INACTIVE) {
+            RedisKeyConst::del(sprintf(RedisKeyConst::ACCESS_BLACK_LIST_KEY, $component->ip_address));
+        }
+        return ['message' => '操作成功！当前ID为：' . $component->id];
     }
 
 
