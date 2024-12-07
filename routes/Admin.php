63ff8ad24e8bbcd9c67880867e7860a73d392fa9
aeiac-home <aeiac@qq.com>
2024-12-07 16:35:06 +0800

    Router-opt：增强

--- routes/Admin.php
+++ routes/Admin.php
@@ -2,37 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Admin\LoginBaseAdminUsersController;
-use App\Http\Controllers\Admin\SystemBaseAdminServicesController;
-use App\Http\Controllers\Admin\SystemBaseAdminPermissionController;
-use App\Http\Controllers\Admin\ArticlesBaseAdminController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\SystemServicesController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\ArticlesController;
 use App\Http\Middleware\Admin\GlobalFunMiddleware as AdminGlobalFunMiddleware;
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     // 登录模块
-    Route::prefix('login')->group(function () {
-        Route::post('/', [LoginBaseAdminUsersController::class, 'login'])->name('admin.login');
-        Route::post('/out', [LoginBaseAdminUsersController::class, 'out'])->name('admin.logout');
+    Route::prefix('auth')->name('auth.')->group(function () {
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/out', [AuthController::class, 'out'])->name('out');
     });
 
     // 系统配置模块
-    Route::prefix('system')->group(function () {
+    Route::prefix('system')->name('system.')->group(function () {
         // IP封禁功能
-        Route::prefix('blacklist')->group(function () {
-            Route::get('/list', [SystemBaseAdminServicesController::class, 'blackList'])->name('admin.system.black.list');
-            Route::post('/save', [SystemBaseAdminServicesController::class, 'blackListSave'])->name('admin.system.black.save');
-        })->name('admin.system.blacklist');
-    })->name('admin.system');
+        Route::prefix('blacklist')->name('blacklist.')->group(function () {
+            Route::get('/list', [SystemServicesController::class, 'blackList'])->name('list');
+            Route::post('/save', [SystemServicesController::class, 'blackListSave'])->name('save');
+        });
+    });
 
     // 权限管理模块
-    Route::prefix('permission')->group(function () {
+    Route::prefix('permission')->name('permission.')->group(function () {
         // 管理员管理
-        Route::get('/admin/users/list', [SystemBaseAdminPermissionController::class, 'adminUserList'])->name('admin.permission.admin.users.list');
+        Route::get('/admin/users/list', [PermissionController::class, 'adminUserList'])->name('admin.users.list');
     });
 
     // 文章管理模块
-    Route::prefix('article')->group(function () {
-        Route::get('/list', [ArticlesBaseAdminController::class, 'list'])->name('admin.article.list');
+    Route::prefix('article')->name('article.')->group(function () {
+        Route::get('/list', [ArticlesController::class, 'list'])->name('list');
 
     });
 })->middleware(AdminGlobalFunMiddleware::class);
