584223b0d37f4290ed202842d5228077e2d5c08b
RayCake <aeiac@qq.com>
2025-05-30 22:33:44 +0800

    迁移-Migrations-Opt：优化封禁表字段

--- database/migrations/2024_07_22_101100_create_blog_system_blacklist_table.php
+++ database/migrations/2024_07_22_101100_create_blog_system_blacklist_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('blog_system_blacklist', function (Blueprint $table) {
             $table->id()->comment('表id');
             $table->string('ip_address',255)->unique()->nullable(false)->comment('IP地址');
-            $table->string('reason',255)->nullable(false)->comment('封禁IP');
+            $table->string('reason',255)->nullable(false)->comment('封禁理由');
             $table->tinyInteger('status')->unsigned()->default(1)->comment('封禁状态:(1:启用, 0:禁用)');
+            $table->tinyInteger('ban_type')->default(1)->comment('封禁类型：1=临时，2=永久');
+            $table->timestamp('expired_at')->nullable()->comment('封禁到期时间（永久封禁为空）');
             $table->timestamps();
 
             $table->comment('系统封禁黑名单表');
